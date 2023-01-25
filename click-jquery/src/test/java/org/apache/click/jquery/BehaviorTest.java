f0d40b4667adb4379b54bc2542a3418bea656c5b
A.Fink <andrey.fink@platform-s.com>
2023-01-26 01:53:35 +0300

    click-jquery small cleanup

--- click-jquery/src/test/java/org/apache/click/jquery/BehaviorTest.java
+++ click-jquery/src/test/java/org/apache/click/jquery/BehaviorTest.java
@@ -4,21 +4,21 @@ import org.apache.click.MockContainer;
 import org.apache.click.jquery.pages.BehaviorPage;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 public class BehaviorTest {
 
-    /**
-     * Test that redirecting to a htm works.
-     */
-    @Test
-    public void testRedirect() {
-        MockContainer container = new MockContainer("web");
-        container.start();
+  /** Test that redirecting to a htm works. */
+  @Test public void testRedirect() {
+    MockContainer container = new MockContainer("web");
+    container.start();
 
-        container.getRequest().setMethod("GET");
+    container.getRequest().setMethod("GET");
 
-        BehaviorPage page = (BehaviorPage) container.testPage(BehaviorPage.class);
+    BehaviorPage page = container.testPage(BehaviorPage.class);
+    assertNotNull(page);
 
-        System.out.println(container.getHtml());
-        container.stop();
-    }
+    System.out.println(container.getHtml());
+    container.stop();
+  }
 }
