d14e160d1ca5308c74cd086b0a75457c999590c0
A.Fink <andrey.fink@platform-s.com>
2023-01-25 15:20:43 +0300

    click-jquery build and test

--- click-jquery/src/main/java/net/sf/click/jquery/behavior/AbstractJQBehavior.java
+++ click-jquery/src/main/java/net/sf/click/jquery/behavior/AbstractJQBehavior.java
@@ -1,18 +1,6 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.sf.click.jquery.behavior;
 
+import lombok.NonNull;
 import org.apache.click.Context;
 import org.apache.click.Control;
 import org.apache.click.Page;
@@ -35,168 +23,162 @@ import java.util.Set;
  */
 public abstract class AbstractJQBehavior extends DefaultAjaxBehavior {
 
-    // Constants --------------------------------------------------------------
 
-    private static final long serialVersionUID = 1L;
+  public static final String BEHAVIOR_MESSAGES = "jq-behavior";
 
-    public static final String BEHAVIOR_MESSAGES = "jq-behavior";
+  /**
+   * The JQuery library (http://jquery.com/):
+   * "<tt>/click-jquery/jquery-1.4.2.js</tt>".
+   */
+  public static String jqueryPath = "/click-jquery/jquery-1.4.2.js";
 
-    /**
-     * The JQuery library (http://jquery.com/):
-     * "<tt>/click-jquery/jquery-1.4.2.js</tt>".
-     */
-    public static String jqueryPath = "/click-jquery/jquery-1.4.2.js";
+  /**
+   * The JQuery Click library:
+   * "<tt>/click-jquery/jquery.click.js</tt>".
+   * <p/>
+   * This library includes JQuery Taconite plugin
+   * (http://www.malsup.com/jquery/taconite/), JQuery LiveQuery plugin
+   * (http://docs.jquery.com/Plugins/livequery)
+   * and utility JavaScript functions.
+   */
+  public static String jqueryClickPath = "/click-jquery/jquery.click.js";
 
-    /**
-     * The JQuery Click library:
-     * "<tt>/click-jquery/jquery.click.js</tt>".
-     * <p/>
-     * This library includes JQuery Taconite plugin
-     * (http://www.malsup.com/jquery/taconite/), JQuery LiveQuery plugin
-     * (http://docs.jquery.com/Plugins/livequery)
-     * and utility JavaScript functions.
-     */
-    public static String jqueryClickPath = "/click-jquery/jquery.click.js";
+  /**
+   * The JQuery blockUI plugin (http://malsup.com/jquery/block/):
+   * "<tt>/click-jquery/blockui/jquery.blockUI.2.31.js</tt>".
+   */
+  public static String blockUIPath = "/click-jquery/blockui/jquery.blockUI.2.31.js";
 
-    /**
-     * The JQuery blockUI plugin (http://malsup.com/jquery/block/):
-     * "<tt>/click-jquery/blockui/jquery.blockUI.2.31.js</tt>".
-     */
-    public static String blockUIPath = "/click-jquery/blockui/jquery.blockUI.2.31.js";
 
-    // Variables --------------------------------------------------------------
 
-    /** Page provides access to its localized messages. */
-    protected Page page;
+  /** Page provides access to its localized messages. */
+  protected Page page;
 
-    /** The Behavior localized messages Map. */
-    protected transient Map messages;
+  /** The Behavior localized messages Map. */
+  protected transient Map<String, String> messages;
 
-    // Public Methods ---------------------------------------------------------
+  // Public Methods ---------------------------------------------------------
 
-    /**
-     * Return a Map of localized messages for this behavior.
-     *
-     * @return a Map of localized messages for the behavior
-     */
-    public Map getMessages() {
-        if (messages == null) {
-            messages = new MessagesMap(getClass(), BEHAVIOR_MESSAGES);
-        }
-        return messages;
+  /**
+   * Return a Map of localized messages for this behavior.
+   *
+   * @return a Map of localized messages for the behavior
+   */
+  public Map<String, String> getMessages() {
+    if (messages == null){
+      messages = new MessagesMap(getClass(), BEHAVIOR_MESSAGES);
+    }
+    return messages;
+  }
+
+  public String getMessage (@NonNull String name) {
+
+    String message = null;
+
+    if (page != null) {
+      message = getPageMessage(page, name);
     }
 
-    public String getMessage(String name) {
-        if (name == null) {
-            throw new IllegalArgumentException("Null name parameter");
-        }
-
-        String message = null;
-
-        if (page != null) {
-            message = getPageMessage(page, name);
-        }
-
-        if (message == null && getMessages().containsKey(name)) {
-            message = (String) getMessages().get(name);
-        }
-
-        return message;
+    if (message == null && getMessages().containsKey(name)){
+      message = getMessages().get(name);
     }
 
-    public String getMessage(String name, Object... args) {
-        String value = getMessage(name);
-        if (value == null) {
-            return null;
-        }
-        return MessageFormat.format(value, args);
+    return message;
+  }
+
+  public String getMessage(String name, Object... args) {
+    String value = getMessage(name);
+    if (value == null) {
+      return null;
+    }
+    return MessageFormat.format(value, args);
+  }
+
+  // Callback Methods -------------------------------------------------------
+
+  @Override
+  public void preRenderHeadElements(Control source) {
+    if (page == null) {
+      // Try and retrieve page from source. If source parent page is not
+      // set, page will be null
+      page = ClickUtils.getParentPage(source);
     }
 
-    // Callback Methods -------------------------------------------------------
+    super.preRenderHeadElements(source);
+  }
 
-    @Override
-    public void preRenderHeadElements(Control source) {
-        if (page == null) {
-            // Try and retrieve page from source. If source parent page is not
-            // set, page will be null
-            page = ClickUtils.getParentPage(source);
-        }
+  // Protected Methods ------------------------------------------------------
 
-        super.preRenderHeadElements(source);
+  protected String getPageMessage(Page page, String name) {
+    String message = null;
+    if (page.getMessages().containsKey(name)) {
+      message = page.getMessages().get(name);
+    }
+    return message;
+  }
+
+  /**
+   * Return the Context of the current request.
+   *
+   * @return the Context of the current request
+   */
+  protected Context getContext() {
+    return Context.getThreadLocalContext();
+  }
+
+  /**
+   * Add a special {@link org.apache.click.element.JsScript} which enables
+   * detailed JavaScript log output to the given headElements list.
+   * <p/>
+   * <b>Please note:</b> use the Firefox browser and Firebug plugin to view
+   * the logged output. However other browsers might also support logging
+   * output.
+   *
+   * @param headElements list which to add the debug script to
+   */
+  protected void addJSDebugScript(List<Element> headElements) {
+    JsScript jsScript = new JsScript();
+    jsScript.setId("enable_js_debugging");
+    if (headElements.contains(jsScript)) {
+      return;
     }
 
-    // Protected Methods ------------------------------------------------------
+    HtmlStringBuffer buffer = new HtmlStringBuffer(100);
+    buffer.append("if (typeof jQuery !== 'undefined') {\n");
+    buffer.append("  if (typeof jQuery.taconite !== 'undefined') {jQuery.taconite.debug=true}\n");
+    buffer.append("  if (typeof Click !== 'undefined' && typeof Click.jq !== 'undefined') {Click.jq.debug = true}\n");
+    buffer.append("}");
+    jsScript.setContent(buffer.toString());
+    headElements.add(jsScript);
+  }
 
-    protected String getPageMessage(Page page, String name) {
-        String message = null;
-        if (page.getMessages().containsKey(name)) {
-            message = (String) page.getMessages().get(name);
-        }
-        return message;
+  /**
+   * Return the Ajax request {@link #parameters} as a serialized URL string.
+   * <p/>
+   * The serialized string will consist of name/value pairs delimited by
+   * the '&' char. An example URL string could be:
+   * "<tt>firstname=John&lastname=Smith&age=12</tt>".
+   *
+   * @return the Ajax request parameters as a serialized URL string
+   */
+  protected String serialize(Map<String, Object> parameters) {
+    HtmlStringBuffer buffer = new HtmlStringBuffer();
+    Set<Entry<String, Object>> entries = parameters.entrySet();
+    for (Iterator<Entry<String, Object>> it = entries.iterator(); it.hasNext(); ) {
+      Entry<String, Object> entry = it.next();
+      String key = entry.getKey();
+      if (key == null) {
+        continue;
+      }
+      Object value = entry.getValue();
+      String encodedValue = ClickUtils.encodeURL(value);
+      buffer.append(key);
+      buffer.append("=");
+      buffer.append(encodedValue);
+      if (it.hasNext()) {
+        buffer.append("&");
+      }
     }
-
-    /**
-     * Return the Context of the current request.
-     *
-     * @return the Context of the current request
-     */
-    protected Context getContext() {
-        return Context.getThreadLocalContext();
-    }
-
-    /**
-     * Add a special {@link org.apache.click.element.JsScript} which enables
-     * detailed JavaScript log output to the given headElements list.
-     * <p/>
-     * <b>Please note:</b> use the Firefox browser and Firebug plugin to view
-     * the logged output. However other browsers might also support logging
-     * output.
-     *
-     * @param headElements list which to add the debug script to
-     */
-    protected void addJSDebugScript(List<Element> headElements) {
-        JsScript jsScript = new JsScript();
-        jsScript.setId("enable_js_debugging");
-        if (headElements.contains(jsScript)) {
-            return;
-        }
-
-        HtmlStringBuffer buffer = new HtmlStringBuffer(100);
-        buffer.append("if (typeof jQuery !== 'undefined') {\n");
-        buffer.append("  if (typeof jQuery.taconite !== 'undefined') {jQuery.taconite.debug=true}\n");
-        buffer.append("  if (typeof Click !== 'undefined' && typeof Click.jq !== 'undefined') {Click.jq.debug = true}\n");
-        buffer.append("}");
-        jsScript.setContent(buffer.toString());
-        headElements.add(jsScript);
-    }
-
-    /**
-     * Return the Ajax request {@link #parameters} as a serialized URL string.
-     * <p/>
-     * The serialized string will consist of name/value pairs delimited by
-     * the '&' char. An example URL string could be:
-     * "<tt>firstname=John&lastname=Smith&age=12</tt>".
-     *
-     * @return the Ajax request parameters as a serialized URL string
-     */
-    protected String serialize(Map<String, Object> parameters) {
-        HtmlStringBuffer buffer = new HtmlStringBuffer();
-        Set<Entry<String, Object>> entries = parameters.entrySet();
-        for (Iterator<Entry<String, Object>> it = entries.iterator(); it.hasNext(); ) {
-            Entry<String, Object> entry = it.next();
-            String key = entry.getKey();
-            if (key == null) {
-                continue;
-            }
-            Object value = entry.getValue();
-            String encodedValue = ClickUtils.encodeURL(value);
-            buffer.append(key);
-            buffer.append("=");
-            buffer.append(encodedValue);
-            if (it.hasNext()) {
-                buffer.append("&");
-            }
-        }
-        return buffer.toString();
-    }
+    return buffer.toString();
+  }
 }
--- click-jquery/src/main/java/net/sf/click/jquery/control/ajax/JQForm.java
+++ click-jquery/src/main/java/net/sf/click/jquery/control/ajax/JQForm.java
@@ -1,19 +1,7 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.sf.click.jquery.control.ajax;
 
 import net.sf.click.jquery.behavior.JQFormBehavior;
+import org.apache.click.Context;
 import org.apache.click.control.Field;
 import org.apache.click.control.Form;
 import org.apache.click.control.HiddenField;
@@ -70,156 +58,156 @@ import java.util.List;
  */
 public class JQForm extends Form {
 
-    // -------------------------------------------------------------- Variables
+  // -------------------------------------------------------------- Variables
 
-    /** The JQuery Behavior object. */
-    protected JQFormBehavior jqBehavior;
+  /** The JQuery Behavior object. */
+  protected JQFormBehavior jqBehavior;
 
-    /** The JavaScript focus function HEAD element. */
-    protected JsScript focusScript;
+  /** The JavaScript focus function HEAD element. */
+  protected JsScript focusScript;
 
-    // ----------------------------------------------------------- Constructors
+  // ----------------------------------------------------------- Constructors
 
-    /**
-     * Create a default JQForm.
-     */
-    public JQForm() {
+  /**
+   * Create a default JQForm.
+   */
+  public JQForm() {
+  }
+
+  /**
+   * Create a JQForm with the given name.
+   *
+   * @param name the name of the form
+   */
+  public JQForm(String name) {
+    super(name);
+  }
+
+  // ------------------------------------------------------ Public Properties
+
+  /**
+   * Return the JQuery Behavior instance.
+   *
+   * @return the jqBehavior instance
+   */
+  public JQFormBehavior getJQBehavior() {
+    if (jqBehavior == null) {
+      jqBehavior = new JQFormBehavior();
     }
+    return jqBehavior;
+  }
 
-    /**
-     * Create a JQForm with the given name.
-     *
-     * @param name the name of the form
-     */
-    public JQForm(String name) {
-        super(name);
+  /**
+   * Set the JQuery Behavior instance.
+   *
+   * @param jqBehavior the JQuery Behavior instance
+   */
+  public void setJQBehavior(JQFormBehavior jqBehavior) {
+    this.jqBehavior = jqBehavior;
+  }
+
+  // --------------------------------------------------------- Public Methods
+
+  /**
+   * Add the {@link #getJQBehavior()} to the control.
+   */
+  @Override
+  public void onInit() {
+    super.onInit();
+    addBehavior(getJQBehavior());
+  }
+
+  /**
+   * Render the link to the given buffer.
+   * <p/>
+   * This method delegates to {@link #addIdField()} to add a hidden field
+   * containing the Form's ID which ensures Ajax requests from this form are
+   * identified.
+   *
+   * @param buffer the buffer to render to
+   */
+  @Override
+  public void render(HtmlStringBuffer buffer) {
+    addIdField();
+    super.render(buffer);
+  }
+
+  // ------------------------------------------------------ Protected Methods
+
+  /**
+   * Adds a hidden Field containing the Form's ID which ensures Ajax requests
+   * from this form can be identified.
+   */
+  protected void addIdField() {
+    // Add the Form ID as a HiddenField to trigger Ajax callback
+    String id = getId();
+    if (getField(id) == null) {
+      add(new HiddenField(id, "1"));
     }
+  }
 
-    // ------------------------------------------------------ Public Properties
+  /**
+   * Return JQForm HEAD elements.
+   *
+   * @return the JQForm HEAD elements
+   */
+  @Override
+  public List<Element> getHeadElements() {
+    if (headElements == null) {
+      headElements = super.getHeadElements();
 
-    /**
-     * Return the JQuery Behavior instance.
-     *
-     * @return the jqBehavior instance
-     */
-    public JQFormBehavior getJQBehavior() {
-        if (jqBehavior == null) {
-             jqBehavior = new JQFormBehavior();
-        }
-        return jqBehavior;
+      if (Context.getThreadLocalContext().isAjaxRequest()) {
+        focusScript = new JsScript();
+        headElements.add(focusScript);
+      }
     }
+    return headElements;
+  }
 
-    /**
-     * Set the JQuery Behavior instance.
-     *
-     * @param jqBehavior the JQuery Behavior instance
-     */
-    public void setJQBehavior(JQFormBehavior jqBehavior) {
-        this.jqBehavior = jqBehavior;
+  /**
+   * Render the Form field focus JavaScript to the string buffer.
+   *
+   * @param buffer the StringBuffer to render to
+   * @param formFields the list of form fields
+   */
+  @Override
+  protected void renderFocusJavaScript(HtmlStringBuffer buffer, List<Field> formFields) {
+    if (Context.getThreadLocalContext().isAjaxRequest()) {
+      HtmlStringBuffer tempBuf = new HtmlStringBuffer();
+      super.renderFocusJavaScript(tempBuf, formFields);
+      String temp = tempBuf.toString();
+      if (StringUtils.isBlank(temp)) {
+        return;
+      }
+      String prefix = "<script type=\"text/javascript\"><!--";
+      temp = temp.substring(prefix.length());
+      String suffix = "//--></script>\n";
+      int end = temp.indexOf(suffix);
+      temp = temp.substring(0, end);
+      focusScript.setContent(temp);
+    } else {
+      super.renderFocusJavaScript(buffer, formFields);
     }
+  }
 
-    // --------------------------------------------------------- Public Methods
-
-    /**
-     * Add the {@link #getJQBehavior()} to the control.
-     */
-    @Override
-    public void onInit() {
-        super.onInit();
-        addBehavior(getJQBehavior());
+  /**
+   * Render the given form start tag and the form hidden fields to the given
+   * buffer.
+   *
+   * @param buffer the HTML string buffer to render to
+   * @param formFields the list of form fields
+   */
+  @Override
+  protected void renderHeader(HtmlStringBuffer buffer, List<Field> formFields) {
+    if (isJavaScriptValidation()) {
+      // The default implementation renders an inline onsubmit handler on the form.
+      // Here we skip rendering that inline onsubmit handler which is instead
+      // handled by the jquery.form.template.js
+      setJavaScriptValidation(false);
+      super.renderHeader(buffer, formFields);
+      setJavaScriptValidation(true);
+    } else {
+      super.renderHeader(buffer, formFields);
     }
-
-    /**
-     * Render the link to the given buffer.
-     * <p/>
-     * This method delegates to {@link #addIdField()} to add a hidden field
-     * containing the Form's ID which ensures Ajax requests from this form are
-     * identified.
-     *
-     * @param buffer the buffer to render to
-     */
-    @Override
-    public void render(HtmlStringBuffer buffer) {
-        addIdField();
-        super.render(buffer);
-    }
-
-    // ------------------------------------------------------ Protected Methods
-
-    /**
-     * Adds a hidden Field containing the Form's ID which ensures Ajax requests
-     * from this form can be identified.
-     */
-    protected void addIdField() {
-        // Add the Form ID as a HiddenField to trigger Ajax callback
-        String id = getId();
-        if (getField(id) == null) {
-            add(new HiddenField(id, "1"));
-        }
-    }
-
-    /**
-     * Return JQForm HEAD elements.
-     *
-     * @return the JQForm HEAD elements
-     */
-    @Override
-    public List<Element> getHeadElements() {
-        if (headElements == null) {
-            headElements = super.getHeadElements();
-
-            if (getContext().isAjaxRequest()) {
-                focusScript = new JsScript();
-                headElements.add(focusScript);
-            }
-        }
-        return headElements;
-    }
-
-    /**
-     * Render the Form field focus JavaScript to the string buffer.
-     *
-     * @param buffer the StringBuffer to render to
-     * @param formFields the list of form fields
-     */
-    @Override
-    protected void renderFocusJavaScript(HtmlStringBuffer buffer, List<Field> formFields) {
-        if (getContext().isAjaxRequest()) {
-            HtmlStringBuffer tempBuf = new HtmlStringBuffer();
-            super.renderFocusJavaScript(tempBuf, formFields);
-            String temp = tempBuf.toString();
-            if (StringUtils.isBlank(temp)) {
-                return;
-            }
-            String prefix = "<script type=\"text/javascript\"><!--";
-            temp = temp.substring(prefix.length());
-            String suffix = "//--></script>\n";
-            int end = temp.indexOf(suffix);
-            temp = temp.substring(0, end);
-            focusScript.setContent(temp);
-        } else {
-            super.renderFocusJavaScript(buffer, formFields);
-        }
-    }
-
-    /**
-     * Render the given form start tag and the form hidden fields to the given
-     * buffer.
-     *
-     * @param buffer the HTML string buffer to render to
-     * @param formFields the list of form fields
-     */
-    @Override
-    protected void renderHeader(HtmlStringBuffer buffer, List formFields) {
-        if (isJavaScriptValidation()) {
-            // The default implementation renders an inline onsubmit handler on form.
-            // Here we skip rendering that inline onsubmit handler which is instead
-            // handled by the jquery.form.template.js
-            setJavaScriptValidation(false);
-            super.renderHeader(buffer, formFields);
-            setJavaScriptValidation(true);
-        } else {
-            super.renderHeader(buffer, formFields);
-        }
-    }
+  }
 }
