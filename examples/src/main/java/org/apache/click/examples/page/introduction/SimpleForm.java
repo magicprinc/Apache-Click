77bc17df75b094fb9fec7ab960ab621248c58230
A.Fink <andrey.fink@platform-s.com>
2023-02-01 00:12:38 +0300

    Field.help demo

--- examples/src/main/java/org/apache/click/examples/page/introduction/SimpleForm.java
+++ examples/src/main/java/org/apache/click/examples/page/introduction/SimpleForm.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.apache.click.examples.page.introduction;
 
 import org.apache.click.control.Form;
@@ -36,33 +18,33 @@ import java.io.Serial;
  * is clicked.
  */
 public class SimpleForm extends BorderPage {
+  @Serial private static final long serialVersionUID = -8294087090143874857L;
 
-    @Serial private static final long serialVersionUID = 1L;
+  private final Form form = new Form("form");
 
-    private Form form = new Form("form");
 
-    // Constructor ------------------------------------------------------------
+  public SimpleForm() {
+    addControl(form);
 
-    public SimpleForm() {
-        addControl(form);
+    var tf = new TextField("name", true);
+    tf.setTitle("Enter anything or your name");
+    tf.setHelp("This is help message ;-)");
+    form.add(tf);
+    form.add(new Submit("OK"));
 
-        form.add(new TextField("name", true));
-        form.add(new Submit("OK"));
+    form.setListener(this, "onSubmit");
+  }
 
-        form.setListener(this, "onSubmit");
+
+  /**
+   * Handle the form submit event.
+   */
+  public boolean onSubmit() {
+    if (form.isValid()) {
+      String msg = "Your name is " + form.getFieldValue("name");
+      addModel("msg", msg);
     }
-
-    // Event Handlers ---------------------------------------------------------
-
-    /**
-     * Handle the form submit event.
-     */
-    public boolean onSubmit() {
-        if (form.isValid()) {
-            String msg = "Your name is " + form.getFieldValue("name");
-            addModel("msg", msg);
-        }
-        return true;
-    }
+    return true;
+  }
 
 }
