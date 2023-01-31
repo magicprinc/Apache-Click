ad76dfc90e1eaf1cf28a89e5bc59f29efeeb56a0
A.Fink <andrey.fink@platform-s.com>
2023-02-01 00:58:52 +0300

    fix PickListDemo pickList.addSelectedValue("004") only if form is fresh new

--- examples/src/main/java/org/apache/click/examples/page/control/PickListDemo.java
+++ examples/src/main/java/org/apache/click/examples/page/control/PickListDemo.java
@@ -1,23 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.apache.click.examples.page.control;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apache.click.control.Form;
 import org.apache.click.control.Option;
 import org.apache.click.control.Submit;
@@ -25,53 +9,60 @@ import org.apache.click.examples.page.BorderPage;
 import org.apache.click.extras.control.PickList;
 
 import java.io.Serial;
-import java.util.List;
 
 /**
- * Provides an Select example Page.
+ * Provides a Select example Page.
  */
+@Slf4j
 public class PickListDemo extends BorderPage {
 
-    @Serial private static final long serialVersionUID = 1L;
+  @Serial private static final long serialVersionUID = 1L;
 
-    private Form form = new Form("form");
+  private final Form form = new Form("form");
 
-    private PickList pickList = new PickList("languages");
+  private final PickList pickList = new PickList("languages");
 
-    // Constructor ------------------------------------------------------------
 
-    public PickListDemo() {
-        addControl(form);
+  public PickListDemo() {
+    addControl(form);
 
-        pickList.setHeaderLabel("Languages", "Selected");
+    pickList.setHeaderLabel("Languages", "Selected");
 
-        pickList.add(new Option("002", "C/C++"));
-        pickList.add(new Option("003", "C#"));
-        pickList.add(new Option("004", "Fortran"));
-        pickList.add(new Option("005", "Java"));
-        pickList.add(new Option("006", "Ruby"));
-        pickList.add(new Option("007", "Perl"));
-        pickList.add(new Option("008", "Visual Basic"));
+    pickList.add(new Option("002", "C/C++"));
+    pickList.add(new Option("003", "C#"));
+    pickList.add(new Option("004", "Fortran"));
+    pickList.add(new Option("005", "Java"));
+    pickList.add(new Option("006", "Ruby"));
+    pickList.add(new Option("007", "Perl"));
+    pickList.add(new Option("008", "Visual Basic"));
 
-        pickList.addSelectedValue("004");
+    log.info("PickListDemo Form was created");
+    form.add(pickList);
 
-        form.add(pickList);
+    form.add(new Submit("ok", " OK ", this, "onOkClick"));
+    val cancel = new Submit("cancel", this, "onCancelClick");
+    cancel.setTitle("Clear selection");
+    form.add(cancel);
+  }
 
-        form.add(new Submit("ok", " OK ", this, "onOkClick"));
-        form.add(new Submit("cancel", this, "onCancelClick"));
+
+  @Override public void onInit () {
+    super.onInit();
+    if (getContext().getRequest().getParameterMap().isEmpty()){
+      pickList.addSelectedValue("004");
     }
+  }//new
 
-    // Event Handlers ---------------------------------------------------------
 
-    public boolean onOkClick() {
-        List selectedValues = pickList.getSelectedValues();
-        addModel("selectedValues", selectedValues);
-        return true;
-    }
+  public boolean onOkClick() {
+    val selectedValues = pickList.getSelectedValues();
+    addModel("selectedValues", selectedValues);
+    return true;
+  }
 
-    public boolean onCancelClick() {
-        pickList.getSelectedValues().clear();
-        return true;
-    }
+  public boolean onCancelClick() {
+    pickList.getSelectedValues().clear();
+    return true;
+  }
 
 }
