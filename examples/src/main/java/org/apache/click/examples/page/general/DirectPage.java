6fe45221f3d4e9ee55f4f1585b57c40ae169d133
A.Fink <andrey.fink@platform-s.com>
2024-12-06 04:32:31 +0300

    fix DirectPage

--- examples/src/main/java/org/apache/click/examples/page/general/DirectPage.java
+++ examples/src/main/java/org/apache/click/examples/page/general/DirectPage.java
@@ -8,8 +8,10 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Serial;
 import java.io.UncheckedIOException;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -20,16 +22,17 @@ import static java.nio.charset.StandardCharsets.*;
  * Provides an example direct <tt>HttpServletResponse</tt> handling.
  */
 public class DirectPage extends Page {
-  private static final long serialVersionUID = 1L;
+  @Serial private static final long serialVersionUID = 1L;
 
   /**
    * Render the Java source file as "text/plain".
    *
    * @see Page#onGet()
    */
-  @Override public void onGet () {
+  @Override
+	public void onGet () {
     val fileName1 = "/readme.txt";// getClass().getName().replace('.', '/');
-    val fileName2 = "/WEB-INF/classes/click-page.properties";
+    val fileName2 = "/click-page.properties";
     val fileName3 = "/META-INF/MANIFEST.MF";
 
     HttpServletResponse response = getContext().getResponse();
@@ -65,7 +68,11 @@ public class DirectPage extends Page {
   }
 
   void downloadFileWithLineNumber (ServletContext context, String fileName, PrintWriter writer) throws IOException {
-    try (val reader = new BufferedReader(new InputStreamReader( context.getResourceAsStream(fileName), UTF_8)) ){
+		InputStream resourceAsStream = context.getResourceAsStream(fileName);
+		if (resourceAsStream == null){// значит он не в META-INF/resources, а просто resources
+			resourceAsStream = getClass().getResourceAsStream(fileName);
+		}
+		try (val reader = new BufferedReader(new InputStreamReader(resourceAsStream, UTF_8)) ){
       String line;  int i = 0;
 
       writer.println("# ");
